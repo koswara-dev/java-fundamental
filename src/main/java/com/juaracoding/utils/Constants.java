651ca071044c1634a0864429bce8da1c43d0a2e6
Koswara <koswara.c79@gmail.com>
2023-06-26 20:11:42 +0700

    update path

--- src/main/java/com/juaracoding/utils/Constants.java
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -2,5 +2,5 @@ package com.juaracoding.utils;
 
 public class Constants {
     public static final String CHROME = "C:\\MyTools\\chromedriver.exe";
-    public static final String FIREFOX = "C:\\MyTools\\geckodriver.exe";
+    public static final String FIREFOX = "E:\\MyTools\\geckodriver.exe";
 }
