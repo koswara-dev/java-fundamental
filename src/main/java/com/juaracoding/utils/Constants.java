7801408dba89deeb9bec117f734f33fa89d8a81e
Koswara <koswara.c79@gmail.com>
2023-06-26 20:04:33 +0700

    add constants

--- src/main/java/com/juaracoding/utils/Constants.java
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -2,4 +2,5 @@ package com.juaracoding.utils;
 
 public class Constants {
     public static final String CHROME = "C:\\MyTools\\chromedriver.exe";
+    public static final String FIREFOX = "C:\\MyTools\\geckodriver.exe";
 }
